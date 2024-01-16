2a8e17f1e646ba50d8ea9e4bcc6693caf6d02586
Aiti <aitijhyasarkar.cse2020@nsec.ac.in>
2024-01-16 15:50:40 +0530

    Fetched 0.0.107

--- src/components/StaticWebPage.js
+++ src/components/StaticWebPage.js
@@ -1,12 +1,21 @@
 import {
-  CoreClasses, CoreContainedButton, CoreH3, CoreH5, CoreImage, CoreStack
+  CoreClasses, CoreContainedButton, CoreH3, CoreH5, CoreImage, CoreStack, coreUseNavigate 
 } from "@wrappid/core";
 
+import logo from "../resources/images/logo_dark.png";
+
 function StaticWebPage() {
+  const navigate = coreUseNavigate();
+  const handleClick1 = () => {
+    navigate("/guide/styles");
+  };
+  const handleClick2 = () => {
+    navigate("/guide/components");
+  };
+
   return (
     <CoreStack styleClasses={[
-      CoreClasses.WIDTH.W_100,
-      CoreClasses.HEIGHT.H_100,
+      CoreClasses.HEIGHT.VH_100,
       CoreClasses.COLOR.TEXT_WHITE,
       CoreClasses.BG.BG_INFO_LIGHT,
       CoreClasses.GAP.GAP_3,
@@ -15,16 +24,26 @@ function StaticWebPage() {
     ]}>
 
       <CoreImage
-        src={"/images/logo.png"}
+        styleClasses={[CoreClasses.HEIGHT.VH_25]}
+        src={logo}
         alt="logo"
-        styleClasses={[CoreClasses.HEIGHT.H_25]}
+        
       />
 
       <CoreH3>Wrappid Framework</CoreH3>
 
       <CoreH5>Wrapper Framework Built to Enable Rapid Development of Applications</CoreH5>
 
-      <CoreContainedButton styleClasses={[CoreClasses.PADDING.PX4, CoreClasses.PADDING.PY2, CoreClasses.BG.BG_WARNING_LIGHT]} size="large" href="/">Get started with Wrappid</CoreContainedButton>
+      <CoreStack direction={"row"}>
+
+        <CoreContainedButton styleClasses={[CoreClasses.PADDING.PX4, CoreClasses.PADDING.PY2, CoreClasses.BG.BG_WARNING_LIGHT]} size="large" OnClick={handleClick1}>Styles</CoreContainedButton>
+
+        <CoreContainedButton styleClasses={[CoreClasses.PADDING.PX4, CoreClasses.PADDING.PY2, CoreClasses.BG.BG_WARNING_LIGHT]} size="large" OnClick={handleClick2}>Components</CoreContainedButton>
+        
+        <CoreContainedButton styleClasses={[CoreClasses.PADDING.PX4, CoreClasses.PADDING.PY2, CoreClasses.BG.BG_WARNING_LIGHT]} size="large" href={"https://github.com/wrappid"}>Get Started</CoreContainedButton>
+     
+      </CoreStack>
+
     </CoreStack>
   );
 }
